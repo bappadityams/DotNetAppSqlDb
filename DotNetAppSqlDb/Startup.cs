a6689b1b172961cff5adb8910b3917231ae89345
Bappaditya Banerjee <104856952+bappadityams@users.noreply.github.com>
2023-03-27 10:45:36 -0400

    Add project files.

---
+++ DotNetAppSqlDb/Startup.cs
@@ -0,0 +1,38 @@
+﻿ using Microsoft.Owin;
+ using Microsoft.Owin.Extensions;
+ using Microsoft.Owin.Security;
+ using Microsoft.Owin.Security.Cookies;
+ using Microsoft.Owin.Security.OpenIdConnect;
+ using Microsoft.IdentityModel.Tokens;
+ using Owin;
+ using System.Configuration;
+
+[assembly: OwinStartup(typeof(DotNetAppSqlDb.Startup))]
+
+namespace DotNetAppSqlDb
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = ConfigurationManager.AppSettings["ClientId"],
+                    Authority = ConfigurationManager.AppSettings["Authority"],
+                    RedirectUri = ConfigurationManager.AppSettings["RedirectUri"],
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "roles"
+                    }
+                });
+
+            // This makes any middleware defined above this line run before the Authorization rule is applied in web.config
+            app.UseStageMarker(PipelineStage.Authenticate);
+        }
+    }
+}
